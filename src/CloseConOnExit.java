f6e5ad3ce432dffd178e9224adcdf829e6519b80
leonard <leonard67130.schmitt@gmail.com>
2020-11-20 23:30:37 +0100

    fin trigger

---
+++ src/CloseConOnExit.java
@@ -0,0 +1,45 @@
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+import java.sql.SQLException;
+
+public class CloseConOnExit implements WindowListener {
+    @Override
+    public void windowOpened(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowClosing(WindowEvent e) {
+        try {
+            SQLControleur.con.close();
+            System.out.println("test");
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        System.exit(0);
+    }
+
+    @Override
+    public void windowClosed(WindowEvent e) {
+    }
+
+    @Override
+    public void windowIconified(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowDeiconified(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowActivated(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowDeactivated(WindowEvent e) {
+
+    }
+}
